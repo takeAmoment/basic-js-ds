bbb9d1555afe080b37c67b6c7998ac3855c236ed
alpekhota <alekspekhota@gmail.com>
2022-05-01 21:28:38 +0200

    feat: create removeKFromList for remove-from-list task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,10 +22,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
+
+function removeKFromList(l, k) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  node = l;
+  arr = [];
+  while(node) {
+    if(node.value !== k){
+      arr.push(node);
+    }
+    
+    node = node.next
+  }
+
+  for(let i = 0; i < arr.length - 1; i++ ){
+    arr[i].next = arr[i + 1];
+  }
+  arr[arr.length - 1].next = null;
+
+  return arr[0];
 }
+
+
 
 module.exports = {
   removeKFromList
